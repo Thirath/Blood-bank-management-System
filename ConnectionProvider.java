fbdd6ea86b6e9e2396985726258a2cc0393061e4
Sardar Thirath singh <73411757+Thirath@users.noreply.github.com>
2023-04-21 19:40:10 +0530

    Add files via upload

---
+++ ConnectionProvider.java
@@ -0,0 +1,20 @@
+package Project;
+import java.sql.*;
+public class ConnectionProvider {
+	
+	public static Connection getCon() 
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodbank","root","R12@singh");
+			return con;
+		}
+		catch(Exception e)
+		{
+			System.out.print(e);
+			return null;
+		}
+	}
+
+}
